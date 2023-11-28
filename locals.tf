f0bdb1931d5549087d0d74965d9559dceb63b4f1
Gesiel1996 <44383183+Gesiel1996@users.noreply.github.com>
2023-11-28 11:14:56 -0300

    Add files via upload

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  common_tags = {
+    
+    owner                 = "Gesiel"
+    managed-by              = "terraform"
+    ambiente                = "dev"
+    Name                    = "storage-account-azure"
+  }
+}
